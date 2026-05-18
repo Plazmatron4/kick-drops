882dcd45b1cd5d0aa2435761c89b295ca5cce21b
Plazmatron4 <114157312+Plazmatron4@users.noreply.github.com>
2026-05-18 17:40:41 +0300

    chore: update app/_layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,7 +24,8 @@ const queryClient = new QueryClient();
 function RootLayoutNav() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
